f9a3c1e9dd6e6279ec26101947c8345b04230831
VGilmanova <ven3890@gmail.com>
2017-04-21 16:23:57 +0300

    Изменен класс "Characteristics.cs"

--- CarKits/Characteristics.cs
+++ CarKits/Characteristics.cs
@@ -8,5 +8,64 @@ namespace CarKits
 {
     class Characteristics
     {
+        private string brand;
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        private string carcase;
+
+        public string Carcase
+        {
+            get { return carcase; }
+            set { carcase = value; }
+        }
+        private int capacity;
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        private int power;
+
+        public int Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+        private int maxspeed;
+
+        public int Maxspeed
+        {
+            get { return maxspeed; }
+            set { maxspeed = value; }
+        }
+
+        private string gearbox;
+
+        public string Gearbox
+        {
+            get { return gearbox; }
+            set { gearbox = value; }
+        }
+        private string engine;
+
+        public string Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+        private double weight;          
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+
     }
 }
