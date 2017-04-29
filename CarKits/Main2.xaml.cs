955367335e5f32fe4e17d3e3804726c378773391
VGilmanova <ven3890@gmail.com>
2017-04-29 22:19:58 +0300

    Добавлено новое окно Main2

---
+++ CarKits/Main2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarKits
+{
+    /// <summary>
+    /// Логика взаимодействия для Main2.xaml
+    /// </summary>
+    public partial class Main2 : Window
+    {
+        public Main2()
+        {
+            InitializeComponent();
+        }
+    }
+}
